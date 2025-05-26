dc9fc6c8835a2ada21faa674bb823f1060785520
Shao Qi Lim <qiqilsq@gmail.com>
2025-05-26 12:44:22 +1000

    Updated name, version, author information, and URL.

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='windfreak',
-    version='0.3.0',
-    author='Christian Hahn',
-    author_email='christianhahn09@gmail.com',
+    name='windfreak-plus',
+    version='0.4.0',
+    author='Shao Qi Lim',
+    author_email='qiqilsq@gmail.com',
     description='Python package for Windfreak Technologies devices.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/christian-hahn/windfreak-python',
+    url='https://github.com/SQLim/windfreak-python.git@SynthNVPro',
     packages=find_packages(exclude=['tests', 'examples']),
     install_requires=[
         'pyserial',
