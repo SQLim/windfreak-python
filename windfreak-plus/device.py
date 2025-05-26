bd5ad4451084845383f7054af1ee57dccdd4f983
Shao Qi Lim <qiqilsq@gmail.com>
2025-05-26 12:43:10 +1000

    Added copyright header.

--- windfreak-plus/device.py
+++ windfreak-plus/device.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 Shao Qi Lim. 
+# Minor modifications to original MIT licensed code.
+# New method in SerialDevice class: dev_clear()
+# Added comments for readability 
+
 from serial import Serial
 
 
@@ -22,16 +27,14 @@ class SerialDevice:
             self._dev = None
 
     def write(self, attribute, *args):
-        # unpacks triple (tuple with three elements), see self.API
-        # _ is an ignored variable in Python (only useful for read function below)
-        dtype, request, _ = self.API[attribute]
+        dtype, request, _ = self.API[attribute] # unpacks tuple of three, _ is ignored
 
         # make sure dtype is a tuple, even if it was initially single 
         # (dtype in self.API['am_lookup_table'] is a tuple)
         dtype = dtype if isinstance(dtype, tuple) else (dtype,)
         
-        # for write, except for am_lookup_table, len(args) = len(dtype) = 1
-        # as for am_lookup_table, len(args) = len(dtype) = 2
+        # len(args) = len(dtype) = 1
+        # except for am_lookup_table, len(args) = len(dtype) = 2
         if len(args) != len(dtype):
             raise ValueError('Number of arguments and data-types are not equal.')
         
@@ -58,30 +61,17 @@ class SerialDevice:
                 expected number based on the attribute's data types, or if
                 an invalid return value is received for a boolean type.
         """
-        # unpacks triple (tuple with three elements), see self.API
-        # _ is an ignored variable in Python (only useful for write function above)
         dtype, _, request = self.API[attribute] 
-
-        # make sure dtype is a tuple, even if it was initially single 
-        # (dtype in self.API['am_lookup_table'] is a tuple)
         dtype = dtype if isinstance(dtype, tuple) else (dtype,)
 
-        # for read, except for am_lookup_table, len(args) = 0, len(args)+1 = len(dtype) = 1
-        # as for am_lookup_table, len(args) = 1, len(args)+1 = len(dtype) = 2
+        # len(args) = 0, len(args)+1 = len(dtype) = 1
+        # except for am_lookup_table, len(args) = 1, len(args)+1 = len(dtype) = 2
         if len(args) + 1 != len(dtype):
             raise ValueError('Must have +1 more data-type than argument.')
 
-        # dt: datatype
-        # ar: argument
-        # If dt is not bool, convert ar to the specified data type dt (e.g., int(ar), float(ar), str(ar))
         args = ((int(ar) if dt is bool else dt(ar)) for dt, ar in zip(dtype, args))
-        
-        # formats request string with args, if any, and passes it to the query method
-        # the response from query is stored in ret (return data)
         ret = self._query(request.format(*args))
-
-        # expected return dtype of read method is the last one (see self.API)
-        dtype = dtype[-1]
+        dtype = dtype[-1] # expected return dtype of read method is the last one (see self.API)
 
         # converts str representation of 0 and 1 to int and checks for errors 
         if dtype is bool:
