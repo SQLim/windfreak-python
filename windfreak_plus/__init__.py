5cfa24c912153830487f59383877bad61323e6d8
Shao Qi Lim <qiqilsq@gmail.com>
2025-05-28 10:49:27 +1000

    renames folder with _

---
+++ windfreak_plus/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Shao Qi Lim. 
+# Minor modifications to original MIT licensed code.
+
+__version__ = '0.4.0'
+
+from .synth_hd import SynthHD
+from .synth_nv_pro import SynthNVPro
