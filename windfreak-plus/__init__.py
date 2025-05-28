0b77f8a49d9ee9214f0242e076f769fade4f22b3
Shao Qi Lim <qiqilsq@gmail.com>
2025-05-28 10:57:53 +1000

    Removed windfreak-plus/

--- windfreak-plus/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2025 Shao Qi Lim. 
-# Minor modifications to original MIT licensed code.
-
-__version__ = '0.4.0'
-
-from .synth_hd import SynthHD
-from .synth_nv_pro import SynthNVPro
